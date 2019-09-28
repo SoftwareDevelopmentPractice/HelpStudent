9bacb1975e6b26953d20b8b2f00918f94a4328f6
fhlxc <1771583929@qq.com>
2019-09-28 16:16:31 +0800

    项目的创建
    项目的创建，目前只有测试的包，以后包的命名都为com.fhlxc.name

---
+++ src/com/fhlxc/test/Test.java
@@ -0,0 +1,17 @@
+package com.fhlxc.test;
+
+/**
+* @author Xingchao Long
+* @date 2019/14/28 16:14:54
+* @ClassName Test.java
+* @Description 类描述
+*/
+
+public class Test {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.print("Test is success!!!");
+    }
+
+}
