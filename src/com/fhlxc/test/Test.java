3e096b7db75ae08fe29ed0c60fea5f254ceb4e5b
fhlxc <1771583929@qq.com>
2019-09-28 16:34:36 +0800

    测试成功

---
+++ src/com/fhlxc/test/Test.java
@@ -0,0 +1,17 @@
+package com.fhlxc.test;
+
+/**
+* @author Xingchao Long
+* @date 2019/33/28 16:33:48
+* @ClassName Test.java
+* @Description 类描述
+*/
+
+public class Test {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.print("Test success!!!");
+    }
+
+}
