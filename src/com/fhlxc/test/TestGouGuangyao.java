338cc1f11d29eef99d2ed81a28736b7119e81e5a
GouGuangyao <1362211091@qq.com>
2019-10-07 21:49:30 +0800

    修改规范
    规范类的定义

--- src/com/fhlxc/test/TestGouGuangyao.java
+++ src/com/fhlxc/test/TestGouGuangyao.java
@@ -3,8 +3,8 @@ package com.fhlxc.test;
 /**
 * @author Guangyao Gou
 * @date 2019/46/07 21:46:16
-* @ClassName TestGouGuangyao.java
-* @Description 类描述
+* @ClassName TestGouGuangyao
+* @Description 苟光耀测试GitHub可视化工具
 */
 
 public class TestGouGuangyao {
