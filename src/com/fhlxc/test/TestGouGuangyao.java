9277d6d2d6b2e3259eb1f9adb57b4357c3fb541d
GouGuangyao <1362211091@qq.com>
2019-10-07 21:47:47 +0800

    苟光耀测试
    测试结果，成功。

---
+++ src/com/fhlxc/test/TestGouGuangyao.java
@@ -0,0 +1,17 @@
+package com.fhlxc.test;
+
+/**
+* @author Guangyao Gou
+* @date 2019/46/07 21:46:16
+* @ClassName TestGouGuangyao.java
+* @Description 类描述
+*/
+
+public class TestGouGuangyao {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("苟光耀测试成功！");
+    }
+
+}
