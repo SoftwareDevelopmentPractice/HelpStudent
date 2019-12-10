b2991ca5173da22304982da3391b3051c56d5530
Xingchao Long <z15881614642@163.com>
2019-12-10 20:13:34 +0800

    主要的界面

---
+++ src/com/fhlxc/gui/ContentJPanel.java
@@ -0,0 +1,17 @@
+package com.fhlxc.gui;
+
+import javax.swing.JPanel;
+
+/**
+* @author Xingchao Long
+* @date 2019/04/10 20:04:20
+* @ClassName ContentJPanel
+* @Description 下方内容面板
+*/
+
+@SuppressWarnings("serial")
+public class ContentJPanel extends JPanel {
+    public ContentJPanel() {
+        setOpaque(false);
+    }
+}
