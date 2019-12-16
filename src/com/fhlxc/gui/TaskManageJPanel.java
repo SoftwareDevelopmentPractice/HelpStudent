3be7917c0c3bbea9d13dd1abf69fab2ab7560dd4
Xingchao Long <z15881614642@163.com>
2019-12-17 02:21:55 +0800

    除导入课表已经全部完成

--- src/com/fhlxc/gui/TaskManageJPanel.java
+++ src/com/fhlxc/gui/TaskManageJPanel.java
@@ -57,32 +57,17 @@ public class TaskManageJPanel extends JPanel {
         setLayout(new BorderLayout(150, 0));
         setFriendJPanel();
         setTaskJPanel();
+        
+        ArrayList<Student> students = new AddTask().find(Data.student.getSt_id());
+        for (Student student: students) {
+            addPartnerInfo(student);
+        }
     }
 
     private void setFriendJPanel() {
         friendJPanel = new JPanel();
         friendJPanel.setOpaque(false);
         friendJPanel.setLayout(new VFlowLayout(VFlowLayout.TOP, 0, 0, false, false));
-
-        
-        Student student = new Student();
-        student.setSt_id("2017141463049");
-        student.setSt_description(
-                "2018年参演电视剧《尼玛死了》");
-
-        addPartnerInfo(student);
-       /* for (int i = 1; i < 20; i++) {
-            Student student = new Student();
-            student.setSt_id("2017141463145");
-            student.setSt_description(
-                    "2004年参演电视剧《与青春有关的日子》，开始在影视圈崭露头角 [1]  。2005年拍摄古装剧《锦衣卫》。2007年主演赵宝刚导演的青春剧《奋斗》； [2]  同年，主演首部电影《走着瞧》。2008年主演滕华涛执导的电视剧《蜗居》，饰演80后城市青年小贝。 [1]  [3]  2009年，在电影《海洋天堂》中扮演自闭症患者王大福；同年参演抗战题材的电视剧《雪豹》 [4]  。2011年，主演的电视剧《裸婚时代》在各大卫视播出； [5]  2011年-2012年连续2年获得北京大学生电影节 [6-7]  最受大学生欢迎男演员奖。2012年，凭借电影《失恋33天》获得第31届大众电影百花奖最佳男主角奖； [8]  同年成立自己经营的北京君竹影视文化有限公司，并导演第一部影视作品《小爸爸》。2013年2月，主演的电影《西游·降魔篇》在全国上映。 [9] \n"
-                            + "2014年3月28日，主演的中韩合资文艺爱情片《我在路上最爱你》在全国上映。2014年12月18日，在姜文执导的动作喜剧片《一步之遥》中扮演武七一角。 [10]  2016年，主演电视剧《少帅》，饰演张学良 [11]  ；主演电视剧《剃刀边缘》 [12]  。7月15日导演的电影《陆垚知马俐》上映。 [13] \n"
-                            + "演艺事业外，文章也参与公益慈善事业，2010年成立大福自闭症关爱基金。\n"
-                            + "2017年9月16日，凭借《陆垚知马俐》获得第31届中国电影金鸡奖导演处女作奖 [14]  。\n"
-                            + "2019年7月28日，文章通过微博宣布，与妻子马伊琍离婚 [15] ");
-
-            addPartnerInfo(student);
-        }*/
 
         scrollPane1 = new JScrollPane();
         scrollPane1.setViewportView(friendJPanel);
@@ -127,8 +112,9 @@ public class TaskManageJPanel extends JPanel {
             dialog.setCursor(new Cursor(Cursor.WAIT_CURSOR));
             // TODO something 点击后,添加右侧的任务信息，调用addTaskInfo()函数
             taskJPanel.removeAll();
-            currPartnerInfoJPanel.getPartner().getSt_id();
-            ArrayList<Task> tasks =new ArrayList<>();
+            String pa_id= currPartnerInfoJPanel.getPartner().getSt_id();
+            String st_id= Data.student.getSt_id();
+            ArrayList<Task> tasks = new AddTask().find(st_id, pa_id);
             for (Task task: tasks) {
                 addTaskInfo(task);
             }
@@ -226,13 +212,6 @@ public class TaskManageJPanel extends JPanel {
                         task.setT_title(modifyTaskJpanel.getTitleField().getText());
                         task.setT_content(modifyTaskJpanel.getContentArea().getText());
                         String str =  modifyTaskJpanel.getTimeField().getText();
-                        /*String rexp = "^((\\d{4}[\\-\\/\\s]([0][1-9]|(1[0-2]))[\\-\\/\\s]([1-9]|([012]\\d)|(3[01]))([ \\t\\n\\x0B\\f\\r])(([0-1]{1}[0-9]{1})|([2]{1}[0-4]{1}))([:])(([0-5]{1}[0-9]{1}|[6]{1}[0]{1}))([:])((([0-5]{1}[0-9]{1}|[6]{1}[0]{1})))";
-                       
-                        if (str != rexp) {
-                            MainWindow.dialog.setDialog("日期格式错误", MainWindow.ERRORIMAGE);
-                            MainWindow.dialog.setVisible(true); 
-                            //return;
-                        }*/
                        
                         SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");                 
                      
@@ -251,8 +230,16 @@ public class TaskManageJPanel extends JPanel {
                         
                         AddTask addTask = new AddTask();
                         addTask.addTask(task, st_id, pa_id, str);
-                        addTaskInfo(task);
+                        taskJPanel.removeAll();
+                        String pa= currPartnerInfoJPanel.getPartner().getSt_id();
+                        String st= Data.student.getSt_id();
+                        ArrayList<Task> tasks = new AddTask().find(st, pa);
+                        for (Task task1: tasks) {
+                            addTaskInfo(task1);
+                        }
                         taskJPanel.updateUI();
+                        MainWindow.dialog.setDialog("任务添加成功", MainWindow.SUCCESSIMAGE);
+                        MainWindow.dialog.setVisible(true);
                         dialog.setVisible(false);
                     }
                 });
@@ -278,7 +265,6 @@ public class TaskManageJPanel extends JPanel {
                 clicked(p);
                 // TODO 删除伙伴
                
-                   
                 String pa_id = p.getPartner().getSt_id();
                 String st_id = Data.student.getSt_id();
                 AddTask deletePartenr = new AddTask();                           
@@ -322,7 +308,7 @@ public class TaskManageJPanel extends JPanel {
 
     public void addTaskInfo(Task task) {
         TaskInfoJPanel taskInfoJPanel = new TaskInfoJPanel(task,
-                MainWindow.initialWidth - MainWindow.initialWidth / 3 - 280, MainWindow.initialHeight / 3, true);
+                MainWindow.initialWidth - MainWindow.initialWidth / 3 - 320, MainWindow.initialHeight / 3, true);
         taskInfoJPanel.getModifyTask().addActionListener(new ActionListener() {
 
             @Override
@@ -358,13 +344,6 @@ public class TaskManageJPanel extends JPanel {
                         task.setT_title(modifytaskJPanel.getTitleField().getText());
                         task.setT_content(modifytaskJPanel.getContentArea().getText());
                         String str =  modifytaskJPanel.getTimeField().getText();
-                        /*String rexp = "^((\\d{4}[\\-\\/\\s]([0][1-9]|(1[0-2]))[\\-\\/\\s]([1-9]|([012]\\d)|(3[01]))([ \\t\\n\\x0B\\f\\r])(([0-1]{1}[0-9]{1})|([2]{1}[0-4]{1}))([:])(([0-5]{1}[0-9]{1}|[6]{1}[0]{1}))([:])((([0-5]{1}[0-9]{1}|[6]{1}[0]{1})))";
-                       
-                        if (str != rexp) {
-                            MainWindow.dialog.setDialog("日期格式错误", MainWindow.ERRORIMAGE);
-                            MainWindow.dialog.setVisible(true); 
-                            //return;
-                        }*/
                        
                         SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");                 
                      
@@ -424,6 +403,22 @@ public class TaskManageJPanel extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // TODO 设置提醒
+                int t_id = task.getT_id();
+                int t_config;
+                if (task.getSt_config()) {
+                    task.setSt_config(false);
+                    taskInfoJPanel.getConfig().setxText("设置提醒");
+                    t_config = 0;
+                } else {
+                    task.setSt_config(true);
+                    taskInfoJPanel.getConfig().setxText("取消提醒");
+                    t_config = 1;
+                }
+                dialog.setCursor(new Cursor(Cursor.WAIT_CURSOR));
+                new AddTask().update(t_id, t_config);
+                taskInfoJPanel.setxText(task);
+                
+                dialog.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
             }
         });
         taskJPanel.add(taskInfoJPanel);
