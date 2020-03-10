206c961c8e204fe498cfdf7f63aba6ed286d54f2
fhlxc <z15881614642@163.com>
2020-03-10 14:27:37 +0800

    test
    天

--- src/com/fhlxc/test/Test.java
+++ src/com/fhlxc/test/Test.java
@@ -10,7 +10,6 @@ package com.fhlxc.test;
 public class Test {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         System.out.print("Test success!!!");
     }
 
